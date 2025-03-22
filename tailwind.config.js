0eb69559b78099cb53dc35a9567139a4e483d65f
Duncan Van Keulen <duncvan@gmail.com>
2025-03-22 15:35:46 -0400

    Fix tailwind include path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ['./**/**/*.{js,jsx,ts,tsx}'],
+  content: ['./app/*.{js,jsx,ts,tsx}'],
   darkMode: ['class'],
   plugins: [
     require('@tailwindcss/container-queries'),
